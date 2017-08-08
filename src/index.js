a28bb1dae1cdd9b3fde05f662828160efad9810d
Scott Rees <reescott@gmail.com>
2017-08-08 12:52:29 -0700

    Fix path to message module

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 // import WVUSForm from './form';
 import InputControl from './controls/input-control';
-import Message from './message.js';
+import Message from './controls/message.js';
 
 export { Message, InputControl }
